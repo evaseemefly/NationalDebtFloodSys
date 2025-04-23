ab5060833b2a38822fcfe3e9c0c52b3de226d45a
evaseemeflt-t7920 <evaseemefly@126.com>
2025-04-23 16:22:34 +0800

    title:25-04-23 台风爬虫微服务脚手架 init

--- delayTasks/common/enums.py
+++ delayTasks/common/enums.py
@@ -1,5 +1,6 @@
 import enum
 
+
 class TaskStatus(enum.Enum):
     """
         任务枚举类
@@ -8,3 +9,23 @@ class TaskStatus(enum.Enum):
     in_progress = 4002
     completed = 4003
     failed = 4004
+
+
+@enum.unique
+class TyphoonForecastSourceEnum(enum.Enum):
+    """
+        台风预报机构
+    """
+    CMA = 201
+
+
+@enum.unique
+class TyphoonOffsetEnum(enum.Enum):
+    """
+        台风偏移枚举
+    """
+    CENTER = 5010
+    FAST = 5011
+    SLOW = 5012
+    RIGHT = 5013
+    LEFT = 5014
